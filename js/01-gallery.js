2f55bad7fb61cc7b309989e13476bfee911eb4a3
Christian Duda <duda.kristian.ns@gmail.com>
2022-01-26 18:39:43 +0200

    Update 01-gallery.js
    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galeryContainer = document.querySelector(".gallery");
+
+// const galeryList = galeryContainer.createElement("ul");
+
+console.log(galeryContainer);
