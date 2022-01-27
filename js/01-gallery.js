41937297148e3f849f09873870ad0960acfea9fc
Christian Duda <duda.kristian.ns@gmail.com>
2022-01-27 14:05:08 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,8 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
-const galeryContainer = document.querySelector(".gallery");
+const galeryContainerEl = document.querySelector(".gallery");
+galeryContainerEl.insertAdjacentHTML("beforeend", "<div><a><img></a></div>");
 
-// const galeryList = galeryContainer.createElement("ul");
+const makeGalery = galleryItems.forEach((item) => {
+  const galeryItemEl = document.querySelector(".gallery > div");
+  galeryItemEl.classList.add("gallery__item");
 
-console.log(galeryContainer);
+  const galeryLinkEl = document.querySelector(".gallery__item > a");
+  galeryLinkEl.classList.add("gallery__link");
+  galeryLinkEl.setAttribute("href", `${item.original}`);
+
+  const galeryImg = document.querySelector(".gallery__link > img");
+  galeryImg.classList.add("gallery__image");
+  galeryImg.setAttribute("data-source", `${item.original}`);
+  galeryImg.setAttribute("alt", `${item.description}`);
+  galeryImg.setAttribute("src", `${item.preview}`);
+  console.log(galeryContainerEl);
+});
