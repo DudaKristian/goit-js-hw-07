0e2196a541cba5ea31c8d6ff7795848889f521a2
Christian Duda <duda.kristian.ns@gmail.com>
2022-01-28 12:10:41 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,37 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 // console.log(galleryItems);
 const galeryContainerEl = document.querySelector(".gallery");
-galeryContainerEl.insertAdjacentHTML("beforeend", "<div><a><img></a></div>");
 
-const makeGalery = galleryItems.forEach((item) => {
-  const galeryItemEl = document.querySelector(".gallery > div");
-  galeryItemEl.classList.add("gallery__item");
+const makeGalery = makeGaleryList(galleryItems);
+galeryContainerEl.insertAdjacentHTML("beforeend", makeGalery);
+// galeryContainerEl.addEventListener("click", onGaleryItemClick);
 
-  const galeryLinkEl = document.querySelector(".gallery__item > a");
-  galeryLinkEl.classList.add("gallery__link");
-  galeryLinkEl.setAttribute("href", `${item.original}`);
+function makeGaleryList(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+      <a class="gallery__link" href='${original}'>
+        <img
+          class="gallery__image"
+          src='${preview}'
+          data-source='${original}'
+          alt='${description}'
+        />
+      </a>
+    </div>
+      `;
+    })
+    .join("");
+}
 
-  const galeryImg = document.querySelector(".gallery__link > img");
-  galeryImg.classList.add("gallery__image");
-  galeryImg.setAttribute("data-source", `${item.original}`);
-  galeryImg.setAttribute("alt", `${item.description}`);
-  galeryImg.setAttribute("src", `${item.preview}`);
-  console.log(galeryContainerEl);
-});
+// function onGaleryItemClick(event) {
+//   event.preventDefault();
+//   const instance = basicLightbox.create(`
+//       <img src="assets/images/image.png" width="800" height="600">
+//   `);
+//   instance.show();
+// }
